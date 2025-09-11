ac2e3630b37497521da0dedbbe66667df3911ebf
rajkumarkushwah3262 <rajkumarkushwah3262@gmail.com>
2025-09-11 22:13:19 +0530

    update

--- src/assets/components/carditems/CardItem.jsx
+++ src/assets/components/carditems/CardItem.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { ShopContext } from "../Context/ShopContext";
+import { ShopContext } from "../../components/Context/ShopContext";
 import remove_icon from '../images/remove_icon.png'
 
 function CardItem() {
