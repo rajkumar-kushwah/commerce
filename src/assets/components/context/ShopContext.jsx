a815638a5c65ad71d0b104fc28f282218a25f190
rajkumarkushwah3262 <rajkumarkushwah3262@gmail.com>
2025-09-14 11:20:21 +0530

    updadte new

--- src/assets/components/context/ShopContext.jsx
+++ src/assets/components/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 import { all_product } from "../Heros/All_product";
 import CardItem from "../carditems/CardItem";
-import product from "../payge/product";
+import product from "../payge/Product"
 
 export const ShopContext = createContext(null);
 const getDefaultCard=()=>{
