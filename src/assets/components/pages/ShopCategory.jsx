f3611076f47613396166916a2aba486e0e918069
rajkumarkushwah3262 <rajkumarkushwah3262@gmail.com>
2025-09-14 12:58:08 +0530

    catagory css

--- src/assets/components/pages/ShopCategory.jsx
+++ src/assets/components/pages/ShopCategory.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
-import "../CSS/shopcategory.css";
+import "../css/shopcategory.css";
 import arrow_down from '../images/arrow-down.png'
 import { Link } from "react-router-dom";
 const ShopCategory = (props) => {
